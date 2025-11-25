30ce6875fe3cd08b95c5e9a551ca22f27adf4971
DevDuulat <duulat.bekm@mail.ru>
2025-11-25 15:56:37 +0600

    feat(route): Add queue submit route

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [QueueController::class, 'index'])->name('queue.index');
-Route::get('/step2', [QueueController::class, 'step2'])->name('queue.index');
+Route::post('/queue/submit', [QueueController::class, 'store'])
+    ->name('queue.submit');
