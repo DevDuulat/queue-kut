b3f90589bac3e43e798b210a141c6752f9c82b7e
DevDuulat <duulat.bekm@mail.ru>
2025-11-25 15:57:21 +0600

    refactor(queue): Improve steps indicator component
    Updates steps indicator for better UI.

--- resources/views/components/queue/steps-indicator.blade.php
+++ resources/views/components/queue/steps-indicator.blade.php
@@ -1,18 +1,17 @@
-{{--Current 1--}}
-<div x-show="currentStage === 1" class="flex justify-center items-center w-full max-w-lg mb-16 relative">
+<div x-show="currentStage === 1" class="flex justify-center items-start w-full max-w-full lg:max-w-lg mb-16 relative">
     <div class="flex flex-col items-center w-1/3 relative z-10">
-        <div class="w-10 h-10 flex items-center justify-center rounded-full bg-yellow-400  text-black font-bold text-lg">1</div>
+        <div class="w-10 h-10 flex items-center justify-center rounded-full bg-yellow-400 text-black font-bold text-lg">1</div>
         <p class="mt-2 text-center text-sm font-semibold text-gray-900">Параметры очереди</p>
     </div>
 
-    <div class="h-0.5 bg-gray-300 absolute left-[calc(1/6*100%-10px)] right-[calc(1/3*100%+10px)] top-5"></div>
+    <div class="h-0.5 bg-gray-300 absolute top-5" style="left: calc(100%/6); right: calc(100%/3);"></div>
 
     <div class="flex flex-col items-center w-1/3 relative z-10">
         <div class="w-10 h-10 flex items-center justify-center rounded-full bg-gray-200 text-gray-500 font-medium text-lg">2</div>
         <p class="mt-2 text-center text-sm text-gray-500">Данные клиента</p>
     </div>
 
-    <div class="h-0.5 bg-gray-300 absolute left-[calc(1/3*100%+10px)] right-[calc(1/6*100%-10px)] top-5"></div>
+    <div class="h-0.5 bg-gray-300 absolute top-5" style="left: calc(100%/3 + 10px); right: calc(100%/6 - 10px);"></div>
 
     <div class="flex flex-col items-center w-1/3 relative z-10">
         <div class="w-10 h-10 flex items-center justify-center rounded-full bg-gray-200 text-gray-500 font-medium text-lg">3</div>
@@ -20,8 +19,7 @@
     </div>
 </div>
 
-{{--Current 2--}}
-<div x-show="currentStage === 2" class="flex justify-center items-center w-full max-w-lg mb-16 relative">
+<div x-show="currentStage === 2" class="flex justify-center items-start w-full max-w-full lg:max-w-lg mb-16 relative">
     <div class="flex flex-col items-center w-1/3 relative z-10">
         <div class="w-10 h-10 flex items-center justify-center rounded-full bg-yellow-400 text-black font-bold text-lg">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -31,22 +29,23 @@
         <p class="mt-2 text-center text-sm font-semibold text-gray-900">Параметры очереди</p>
     </div>
 
-    <div class="h-0.5 bg-yellow-400 absolute left-[16.66%] right-[50%] top-5"></div>
+    <div class="h-0.5 bg-yellow-400 absolute top-5" style="left: calc(100%/6); right: calc(100%/3);"></div>
 
     <div class="flex flex-col items-center w-1/3 relative z-10">
         <div class="w-10 h-10 flex items-center justify-center rounded-full bg-yellow-400 text-black font-bold text-lg">2</div>
         <p class="mt-2 text-center text-sm font-semibold text-gray-900">Данные клиента</p>
     </div>
 
-    <div class="h-0.5 bg-gray-300 absolute left-[50%] right-[16.66%] top-5"></div>
+    <div class="h-0.5 bg-yellow-400 absolute top-5" style="left: calc(100%/3 + 10px); right: calc(100%/3 + 10px);"></div>
+    <div class="h-0.5 bg-gray-300 absolute top-5" style="left: calc(100%/2); right: calc(100%/6 - 10px);"></div>
 
     <div class="flex flex-col items-center w-1/3 relative z-10">
         <div class="w-10 h-10 flex items-center justify-center rounded-full bg-gray-200 text-gray-500 font-medium text-lg">3</div>
         <p class="mt-2 text-center text-sm text-gray-500">Условия оплаты</p>
     </div>
 </div>
-{{--Current 3--}}
-<div x-show="currentStage === 3" class="flex justify-center items-center w-full max-w-lg mb-16 relative">
+
+<div x-show="currentStage === 3" class="flex justify-center items-start w-full max-w-full lg:max-w-lg mb-16 relative">
     <div class="flex flex-col items-center w-1/3 relative z-10">
         <div class="w-10 h-10 flex items-center justify-center rounded-full bg-yellow-400 text-black font-bold text-lg">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -56,7 +55,7 @@
         <p class="mt-2 text-center text-sm font-semibold text-gray-900">Параметры очереди</p>
     </div>
 
-    <div class="h-0.5 bg-yellow-400 absolute left-[16.66%] right-[50%] top-5"></div>
+    <div class="h-0.5 bg-yellow-400 absolute top-5" style="left: calc(100%/6); right: calc(100%/3);"></div>
 
     <div class="flex flex-col items-center w-1/3 relative z-10">
         <div class="w-10 h-10 flex items-center justify-center rounded-full bg-yellow-400 text-black font-bold text-lg">
@@ -67,7 +66,7 @@
         <p class="mt-2 text-center text-sm font-semibold text-gray-900">Данные клиента</p>
     </div>
 
-    <div class="h-0.5 bg-yellow-400 absolute left-[50%] right-[16.66%] top-5"></div>
+    <div class="h-0.5 bg-yellow-400 absolute top-5" style="left: calc(100%/3 + 10px); right: calc(100%/6 - 10px);"></div>
 
     <div class="flex flex-col items-center w-1/3 relative z-10">
         <div class="w-10 h-10 flex items-center justify-center rounded-full bg-yellow-400 text-black font-bold text-lg">3</div>
