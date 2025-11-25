4578c28162ceec84a51638938b9404bb6755f463
DevDuulat <duulat.bekm@mail.ru>
2025-11-25 15:56:11 +0600

    feat(queue): Add Queue model and migration

---
+++ app/Models/Queue.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Queue extends Model
+{
+    use HasFactory;
+
+    protected $table = 'queue';
+
+    protected $fillable = [
+        'queue_type',
+        'apartment_type',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'curator_id',
+        'inn',
+        'document_series',
+        'document_number',
+        'issued_by',
+        'monthly_payment_no_down',
+        'custom_monthly_payment',
+        'down_payment',
+        'payment_term',
+        'issue_date',
+    ];
+
+    protected $casts = [
+        'queue_type' => 'integer',
+        'apartment_type' => 'integer',
+        'custom_monthly_payment' => 'integer',
+        'down_payment' => 'integer',
+        'payment_term' => 'integer',
+        'issue_date' => 'date',
+    ];
+}
---
+++ database/migrations/2025_11_25_053859_create_queue_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('queue', function (Blueprint $table) {
+            $table->id();
+            $table->tinyInteger('queue_type');
+            $table->tinyInteger('apartment_type');
+            $table->string('first_name', 100);
+            $table->string('last_name', 100);
+            $table->string('phone_number', 20);
+            $table->string('curator_id', 50)->nullable();
+            $table->string('inn', 14)->unique();
+            $table->string('document_series', 10)->nullable();
+            $table->string('document_number', 7)->unique();
+            $table->string('issued_by', 255);
+            $table->string('monthly_payment_no_down', 20)->nullable();
+            $table->unsignedInteger('custom_monthly_payment')->nullable();
+            $table->unsignedBigInteger('down_payment')->nullable();
+            $table->unsignedSmallInteger('payment_term')->nullable();
+            $table->date('issue_date');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('queue');
+    }
+};
