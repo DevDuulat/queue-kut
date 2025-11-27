7a37f99f8bc9e6bf2fc9f5b8f86ea0c0c8871318
DevDuulat <duulat.bekm@mail.ru>
2025-11-27 14:18:58 +0600

    feat(route): Apply rate limiting to submit route
    Adds throttle middleware to queue submit route.

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\QueueController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Routing\Middleware\ThrottleRequests;
 
 
 Route::get('/', [QueueController::class, 'index'])->name('queue.index');
-Route::post('/queue/submit', [QueueController::class, 'store'])
-    ->name('queue.submit');
-Route::get('/queue/download-ticket', [QueueController::class, 'downloadTicket'])->name('queue.download_ticket');
 
+Route::post('/queue/submit', [QueueController::class, 'store'])
+    ->middleware('throttle:10,1')
+    ->name('queue.submit');
+
+Route::get('/queue/download-ticket', [QueueController::class, 'downloadTicket'])
+    ->name('queue.download_ticket');
